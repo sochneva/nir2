045dfd56bdf9ec1553c25ece0a59d04a2be6c6ab
Ekaterina Sochneva <kat.so4neva@yandex.ru>
2021-03-01 23:39:18 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace nir2
+{
+    partial class nir2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GvdLabel = new System.Windows.Forms.Label();
+            this.TvdLabel = new System.Windows.Forms.Label();
+            this.PvdLabel = new System.Windows.Forms.Label();
+            this.GndLabel = new System.Windows.Forms.Label();
+            this.TndLabel = new System.Windows.Forms.Label();
+            this.PndLabel = new System.Windows.Forms.Label();
+            this.PkLabel = new System.Windows.Forms.Label();
+            this.NLabel = new System.Windows.Forms.Label();
+            this.DkLabel = new System.Windows.Forms.Label();
+            this.GvdTrackBar = new System.Windows.Forms.TrackBar();
+            this.TvdTrackBar = new System.Windows.Forms.TrackBar();
+            this.PvdTrackBar = new System.Windows.Forms.TrackBar();
+            this.GndTrackBar = new System.Windows.Forms.TrackBar();
+            this.TndTrackBar = new System.Windows.Forms.TrackBar();
+            this.PndTrackBar = new System.Windows.Forms.TrackBar();
+            this.PkTrackBar = new System.Windows.Forms.TrackBar();
+            this.NTextBox = new System.Windows.Forms.TextBox();
+            this.DkTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GvdTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TvdTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PvdTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GndTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TndTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PndTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PkTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GvdLabel
+            // 
+            this.GvdLabel.AutoSize = true;
+            this.GvdLabel.Location = new System.Drawing.Point(12, 15);
+            this.GvdLabel.Name = "GvdLabel";
+            this.GvdLabel.Size = new System.Drawing.Size(173, 13);
+            this.GvdLabel.TabIndex = 0;
+            this.GvdLabel.Text = "Расход пара высокого давления";
+            // 
+            // TvdLabel
+            // 
+            this.TvdLabel.AutoSize = true;
+            this.TvdLabel.Location = new System.Drawing.Point(12, 66);
+            this.TvdLabel.Name = "TvdLabel";
+            this.TvdLabel.Size = new System.Drawing.Size(204, 13);
+            this.TvdLabel.TabIndex = 1;
+            this.TvdLabel.Text = "Температура пара высокого давления";
+            // 
+            // PvdLabel
+            // 
+            this.PvdLabel.AutoSize = true;
+            this.PvdLabel.Location = new System.Drawing.Point(12, 117);
+            this.PvdLabel.Name = "PvdLabel";
+            this.PvdLabel.Size = new System.Drawing.Size(188, 13);
+            this.PvdLabel.TabIndex = 2;
+            this.PvdLabel.Text = "Давление пара высокого давления";
+            // 
+            // GndLabel
+            // 
+            this.GndLabel.AutoSize = true;
+            this.GndLabel.Location = new System.Drawing.Point(12, 168);
+            this.GndLabel.Name = "GndLabel";
+            this.GndLabel.Size = new System.Drawing.Size(165, 13);
+            this.GndLabel.TabIndex = 3;
+            this.GndLabel.Text = "Расход пара низкого давления";
+            // 
+            // TndLabel
+            // 
+            this.TndLabel.AutoSize = true;
+            this.TndLabel.Location = new System.Drawing.Point(12, 219);
+            this.TndLabel.Name = "TndLabel";
+            this.TndLabel.Size = new System.Drawing.Size(196, 13);
+            this.TndLabel.TabIndex = 4;
+            this.TndLabel.Text = "Температура пара низкого давления";
+            // 
+            // PndLabel
+            // 
+            this.PndLabel.AutoSize = true;
+            this.PndLabel.Location = new System.Drawing.Point(12, 270);
+            this.PndLabel.Name = "PndLabel";
+            this.PndLabel.Size = new System.Drawing.Size(180, 13);
+            this.PndLabel.TabIndex = 5;
+            this.PndLabel.Text = "Давление пара низкого давления";
+            // 
+            // PkLabel
+            // 
+            this.PkLabel.AutoSize = true;
+            this.PkLabel.Location = new System.Drawing.Point(12, 321);
+            this.PkLabel.Name = "PkLabel";
+            this.PkLabel.Size = new System.Drawing.Size(141, 13);
+            this.PkLabel.TabIndex = 6;
+            this.PkLabel.Text = "Давление в конденсаторе";
+            // 
+            // NLabel
+            // 
+            this.NLabel.AutoSize = true;
+            this.NLabel.Location = new System.Drawing.Point(501, 15);
+            this.NLabel.Name = "NLabel";
+            this.NLabel.Size = new System.Drawing.Size(193, 13);
+            this.NLabel.TabIndex = 7;
+            this.NLabel.Text = "Электрическая мощность ПТ брутто";
+            // 
+            // DkLabel
+            // 
+            this.DkLabel.AutoSize = true;
+            this.DkLabel.Location = new System.Drawing.Point(501, 66);
+            this.DkLabel.Name = "DkLabel";
+            this.DkLabel.Size = new System.Drawing.Size(147, 13);
+            this.DkLabel.TabIndex = 8;
+            this.DkLabel.Text = "Расход пара в конденсатор";
+            // 
+            // GvdTrackBar
+            // 
+            this.GvdTrackBar.Location = new System.Drawing.Point(222, 15);
+            this.GvdTrackBar.Name = "GvdTrackBar";
+            this.GvdTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.GvdTrackBar.TabIndex = 9;
+            // 
+            // TvdTrackBar
+            // 
+            this.TvdTrackBar.Location = new System.Drawing.Point(222, 66);
+            this.TvdTrackBar.Name = "TvdTrackBar";
+            this.TvdTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.TvdTrackBar.TabIndex = 10;
+            // 
+            // PvdTrackBar
+            // 
+            this.PvdTrackBar.Location = new System.Drawing.Point(222, 117);
+            this.PvdTrackBar.Name = "PvdTrackBar";
+            this.PvdTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.PvdTrackBar.TabIndex = 11;
+            // 
+            // GndTrackBar
+            // 
+            this.GndTrackBar.Location = new System.Drawing.Point(222, 168);
+            this.GndTrackBar.Name = "GndTrackBar";
+            this.GndTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.GndTrackBar.TabIndex = 12;
+            // 
+            // TndTrackBar
+            // 
+            this.TndTrackBar.Location = new System.Drawing.Point(222, 219);
+            this.TndTrackBar.Name = "TndTrackBar";
+            this.TndTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.TndTrackBar.TabIndex = 13;
+            // 
+            // PndTrackBar
+            // 
+            this.PndTrackBar.Location = new System.Drawing.Point(222, 270);
+            this.PndTrackBar.Name = "PndTrackBar";
+            this.PndTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.PndTrackBar.TabIndex = 14;
+            // 
+            // PkTrackBar
+            // 
+            this.PkTrackBar.Location = new System.Drawing.Point(222, 321);
+            this.PkTrackBar.Name = "PkTrackBar";
+            this.PkTrackBar.Size = new System.Drawing.Size(204, 45);
+            this.PkTrackBar.TabIndex = 15;
+            // 
+            // NTextBox
+            // 
+            this.NTextBox.Location = new System.Drawing.Point(701, 12);
+            this.NTextBox.Name = "NTextBox";
+            this.NTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NTextBox.TabIndex = 16;
+            // 
+            // DkTextBox
+            // 
+            this.DkTextBox.Location = new System.Drawing.Point(701, 63);
+            this.DkTextBox.Name = "DkTextBox";
+            this.DkTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DkTextBox.TabIndex = 17;
+            // 
+            // nir2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 369);
+            this.Controls.Add(this.DkTextBox);
+            this.Controls.Add(this.NTextBox);
+            this.Controls.Add(this.PkTrackBar);
+            this.Controls.Add(this.PndTrackBar);
+            this.Controls.Add(this.TndTrackBar);
+            this.Controls.Add(this.GndTrackBar);
+            this.Controls.Add(this.PvdTrackBar);
+            this.Controls.Add(this.TvdTrackBar);
+            this.Controls.Add(this.GvdTrackBar);
+            this.Controls.Add(this.DkLabel);
+            this.Controls.Add(this.NLabel);
+            this.Controls.Add(this.PkLabel);
+            this.Controls.Add(this.PndLabel);
+            this.Controls.Add(this.TndLabel);
+            this.Controls.Add(this.GndLabel);
+            this.Controls.Add(this.PvdLabel);
+            this.Controls.Add(this.TvdLabel);
+            this.Controls.Add(this.GvdLabel);
+            this.Name = "nir2";
+            this.Text = "Расчет ПТ";
+            ((System.ComponentModel.ISupportInitialize)(this.GvdTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TvdTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PvdTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GndTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TndTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PndTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PkTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label GvdLabel;
+        private System.Windows.Forms.Label TvdLabel;
+        private System.Windows.Forms.Label PvdLabel;
+        private System.Windows.Forms.Label GndLabel;
+        private System.Windows.Forms.Label TndLabel;
+        private System.Windows.Forms.Label PndLabel;
+        private System.Windows.Forms.Label PkLabel;
+        private System.Windows.Forms.Label NLabel;
+        private System.Windows.Forms.Label DkLabel;
+        private System.Windows.Forms.TrackBar GvdTrackBar;
+        private System.Windows.Forms.TrackBar TvdTrackBar;
+        private System.Windows.Forms.TrackBar PvdTrackBar;
+        private System.Windows.Forms.TrackBar GndTrackBar;
+        private System.Windows.Forms.TrackBar TndTrackBar;
+        private System.Windows.Forms.TrackBar PndTrackBar;
+        private System.Windows.Forms.TrackBar PkTrackBar;
+        private System.Windows.Forms.TextBox NTextBox;
+        private System.Windows.Forms.TextBox DkTextBox;
+    }
+}
+
